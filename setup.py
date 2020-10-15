4cf00f80a60b520c9252cd39626f8c283e977c21
Gabriela Plantie <glplantie@gmail.com>
2020-10-14 22:23:35 -0300

    pkg

---
+++ setup.py
@@ -0,0 +1,28 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="evaluation_tools",
+    version="0.0.1",
+    author="Gabriela Plantie",
+    author_email="glplantie@gmail.com",
+    description="Evaluation Tools",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gabriela-plantie/evaluation_tools",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        'numpy>=1.19.2',
+        'pandas>=1.1.2',
+        'statsmodels>=0.12.0'
+        'scipy>=1.5.2'
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
